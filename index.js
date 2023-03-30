dafdf24eec26726034aa509912be34c36ee59b68
Chuyang Li <19cl98@queensu.ca>
2023-03-30 17:20:18 -0400

    update

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
+$(document).ready(function () {
+    $("#links a").on("click", function (event) {
         event.preventDefault();
-        var hash = this.hash;
-        $('body,html').animate({
-        scrollTop: $(hash).offset().top
-        }, 1200, function(){
-        window.location.hash = hash;
-       });
-       } 
-      });
-  });
-  
-  var width = $(window).width(); 
-  
-  window.onscroll = function(){
-  if ((width >= 900)){
-      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-          $("#middle").css("background-size","150% auto");
-      }else{
-          $("#middle").css("background-size","100% auto");        
-      }
-  }
-  };
-  
-  setTimeout(function(){
-      $("#loading").addClass("animated fadeOut");
-      setTimeout(function(){
-        $("#loading").removeClass("animated fadeOut");
-        $("#loading").css("display","none");
-      },800);
-  },1450);
+        const targetId = this.hash;
+        const targetElement = $(targetId);
+        const targetPosition = targetElement.offset().top;
+        const offset = $(window).height() * 0.5;
+        const finalPosition = targetPosition - offset;
+
+        $("body,html").animate(
+            {
+                scrollTop: finalPosition,
+            },
+            1200,
+            function () {
+                window.location.hash = targetId;
+            }
+        );
+    });
+
+    var width = $(window).width();
+
+    window.onscroll = function () {
+        if (width >= 900) {
+            if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+                $("#middle").css("background-size", "150% auto");
+            } else {
+                $("#middle").css("background-size", "100% auto");
+            }
+        }
+    };
+
+    setTimeout(function () {
+        $("#loading").addClass("animated fadeOut");
+        setTimeout(function () {
+            $("#loading").removeClass("animated fadeOut");
+            $("#loading").css("display", "none");
+        }, 800);
+    }, 1450);
+});
